929f1dfe780c2e8a1c7baa02c82570b60b47ca8d
lvjaiwei1014 <lvjiawei1014@hotmail.com>
2019-11-17 14:33:10 +0800

    Initial commit

---
+++ UniversalComputingPlatform/Core.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalComputingPlatform
+{
+    public class Core
+    {
+
+    }
+
+    public class UniversalObject
+    {
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+
+    public abstract class UniversalTool:UniversalObject
+    {
+        public abstract void Run();
+
+    }
+
+    public class UniversalData : UniversalObject
+    {
+
+    }
+}
